2d594ed0f02bbb5c7d612f34e44686e180a1b99a
salaniz <salaniz@users.noreply.github.com>
2022-07-28 11:50:24 +0000

    Update dataset links

--- download_data.sh
+++ download_data.sh
@@ -2,11 +2,11 @@ mkdir -p ./data
 cd ./data
 
 # Download and extract ShoeV2 and ChairV2
-wget https://nc.mlcloud.uni-tuebingen.de/index.php/s/sDxWekWPQGcstzK/download -O datav2.tar
-tar xvf datav2.tar
-rm datav2.tar
+wget https://nc.mlcloud.uni-tuebingen.de/index.php/s/ZsydaSGocdxSy2k/download -O datav2.tar.gz
+tar xvf datav2.tar.gz
+rm datav2.tar.gz
 
 # Download and extract Quickdraw09
-wget https://nc.mlcloud.uni-tuebingen.de/index.php/s/GQfxJXZosJLjaJS/download -O quickdraw09.tar
-tar xvf quickdraw09.tar
-rm quickdraw09.tar
+wget https://nc.mlcloud.uni-tuebingen.de/index.php/s/JLeNSFQdGgkEPet/download -O quickdraw09.tar.gz
+tar xvf quickdraw09.tar.gz
+rm quickdraw09.tar.gz
